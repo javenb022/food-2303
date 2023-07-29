faf56f8b0a07e56e561c5ce6fa485eebe99ee51c
Javen Wilson <javenb022@gmail.com>
2023-07-29 08:33:13 -0400

    Fix test

--- spec/poros/food_spec.rb
+++ spec/poros/food_spec.rb
@@ -2,17 +2,17 @@ require "rails_helper"
 # Test won't pass because upc and brand owner attributes are coming up as nil
 # And I'm not sure why or how to fix it. Google hasn't helped me either
 RSpec.describe Food do
-  xit "exists with attributes" do
-    food_item = Food.new({
-                      brand_owner: 'Hostess',
+  it "exists with attributes" do
+    food = Food.new({
+                      brandOwner: 'Hostess',
                       description: 'Donut',
                       ingredients: 'Dough',
-                      upc: 123456789
+                      gtinUpc: 123456789
                     })
 
     expect(food).to be_a(Food)
-    expect(food.upc).to be_an(String)
-    expect(food.upc).to eq("123456789")
+    expect(food.upc).to be_an(Integer)
+    expect(food.upc).to eq(123456789)
     expect(food.description).to be_a(String)
     expect(food.description).to eq("Donut")
     expect(food.brand_owner).to be_a(String)
